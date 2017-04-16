e42974a0ec15237582835a60f978c3be02bdf113
YANG WANG <yang.wang.dec@gmail.com>
2017-04-16 17:14:38 -0400

    first commit

---
+++ Assets/Drag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.frameCount % 100 == 0) {
+			StartCoroutine ("DragMe");
+		}
+	}
+
+
+	IEnumerator DragMe() {
+		for (int i = 0; i < 10; i++) {
+			transform.Translate (new Vector3 (0, 0, -0.4f), Space.World);
+			transform.RotateAround (transform.position, Vector3.right, 10.0f);
+			yield return new WaitForSeconds (0.01f);
+		}
+		transform.RotateAround (transform.position,Vector3.right, -100.0f);
+	}
+
+		
+}
---
+++ Assets/DrawLine.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+
+	static Material lineMaterial;
+	public int lineCount = 100;
+	public float radius = 3.0f;
+
+
+	static void CreateLineMaterial()
+	{
+		if (!lineMaterial)
+		{
+			// Unity has a built-in shader that is useful for drawing
+			// simple colored things.
+			Shader shader = Shader.Find("Hidden/Internal-Colored");
+			lineMaterial = new Material(shader);
+			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+			// Turn on alpha blending
+			lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+			lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+			// Turn backface culling off
+			lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+			// Turn off depth writes
+			lineMaterial.SetInt("_ZWrite", 0);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnRenderObject(){
+		CreateLineMaterial ();
+//		Debug.Log ("onpostrender");
+//		GetComponent<Renderer>().material = lineMaterial;
+
+		lineMaterial.SetPass (0);
+
+		GL.PushMatrix ();
+		GL.MultMatrix (transform.localToWorldMatrix);
+
+		GL.Begin (GL.LINES);
+		for (int i = 0; i < lineCount; ++i)
+		{
+			float a = i / (float)lineCount;
+			float angle = a * Mathf.PI * 2;
+			// Vertex colors change from red to green
+			GL.Color(new Color(a, 1 - a, 0, 0.8F));
+			// One vertex at transform position
+			GL.Vertex3(0, 0, 0);
+			// Another vertex at edge of circle
+			GL.Vertex3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
+		}
+		GL.End();
+		GL.PopMatrix ();
+	}
+
+}
